163dad7ed6052e7a3105f71910cd1fce7306c05e
Thales Machado <thales.machado@taqtile.com>
2014-01-17 20:03:09 -0200

    configuring release

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,13 @@
 #!/bin/sh
 set -e
 
+RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
+RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER - Uploaded: $RELEASE_DATE"
+
 curl "http://testflightapp.com/api/builds.json"  \
     -F file=@"$TRAVIS_BUILD_DIR/MyFirstApp/build/apk/MyFirstApp-release.apk" \
     -F api_token=$TESTFLIGHT_API_TOKEN \
     -F team_token=$TESTFLIGHT_TEAM_TOKEN \
-    -F notes="Build by Travis CI" \
-    -F notify=True \
-    -F distribution_lists="Internal"
+    -F notify=true \
+    -F distribution_lists="$TESTFLIGHT_DISTRIBUTION_LIST" \
+    -F notes="$RELEASE_NOTES"
