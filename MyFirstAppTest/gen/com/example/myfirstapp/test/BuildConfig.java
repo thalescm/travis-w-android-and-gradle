156438828008d425eb9a3b6309044d8760a77880
Thales Machado <thales.machado@taqtile.com>
2014-01-20 16:29:41 -0200

    configuring

---
+++ MyFirstAppTest/gen/com/example/myfirstapp/test/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.example.myfirstapp.test;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 set -e
 
+#!/bin/sh
+if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then
+  echo "This is a pull request. No deployment will be done."
+  exit 0
+fi
+if [[ "$TRAVIS_BRANCH" != "master" ]]; then
+  echo "Testing on a branch other than master. No deployment will be done."
+  exit 0
+fi
+
 RELEASE_DATE=`date '+%Y-%m-%d %H:%M:%S'`
 RELEASE_NOTES="Build: $TRAVIS_BUILD_NUMBER - Uploaded: $RELEASE_DATE"
 
