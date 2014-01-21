cac43d6f49fa39f1a6b2391a23a2e6a7d91263ef
Thales Machado <thales.machado@taqtile.com>
2014-01-21 14:57:12 -0200

    agora entao, eh certeza

--- scripts/test.sh
+++ scripts/test.sh
@@ -7,4 +7,4 @@ echo "y" | $PWD/android-sdk-linux/tools/android update sdk --filter $ANDROID_SDK
 
 # Create and start emulator
 echo no | $PWD/android-sdk-linux/tools/android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI;
-$PWD/android-sdk-linux/tools/emulator -avd test -no-skin -no-audio -no-window &;
+$PWD/android-sdk-linux/tools/emulator -avd test -no-skin -no-audio -no-window &
