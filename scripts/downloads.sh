21243f6f52ab6ee688ef5f688683e1de80edd06d
Thales Machado <thales.machado@taqtile.com>
2014-01-21 17:33:12 -0200

    adding android path into downloads.sh

--- scripts/downloads.sh
+++ scripts/downloads.sh
@@ -39,9 +39,5 @@ mkdir -p $ANDROID_HOME/build-tools/;
 mv $ANDROID_HOME/android-$ANDROID_OS_VERSION $ANDROID_HOME/build-tools/$ANDROID_BUILD_TOOLS_VERSION;
 
 #- android list sdk -a -e
-echo "y" | android update sdk --filter platform-tool,tool --no-ui --force > /dev/null;
-echo "y" | android update sdk --filter $ANDROID_SDKS --no-ui --force > /dev/null;
-
-# Create and start emulator
-echo no | android create avd --force -n test -t $ANDROID_TARGET --abi $ANDROID_ABI;
-emulator -avd test -no-skin -no-audio -no-window &
+echo "y" | $ANDROID_HOME/tools/android update sdk --filter platform-tool,tool --no-ui --force > /dev/null;
+echo "y" | $ANDROID_HOME/tools/android update sdk --filter $ANDROID_SDKS --no-ui --force > /dev/null;
